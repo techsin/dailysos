a465f3782ad152924166375b7d074f8e3b48d8f8
techsin <myemailum14@gmail.com>
2017-10-22 10:58:01 -0400

    redirectFakelogin

--- backend/controllers/register.js
+++ backend/controllers/register.js
@@ -4,5 +4,8 @@ const router = express.Router();
 router.get('/', function(req, res){
     res.render('register');
 });
+router.post('/', function(req, res){
+    res.redirect('/timeline');
+});
 
 module.exports = router;
