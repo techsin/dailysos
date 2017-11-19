2c40abe6f40b0151a7df734eaa45ee8572550f6a
Vish Misir <vmisir000@citymail.cuny.edu>
2017-11-18 20:22:19 -0500

    login template

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -2,15 +2,20 @@ const express = require('express');
 const router = express.Router();
 const passport = require('../middlewares/auth');
 
-
 router.get('/', function (req, res) {
     res.render('login');
 });
+
 router.post('/', function (req, res) {
     passport.authenticate('local', {
         successRedirect: '/timeline',
         failureRedirect: '/login',
     })(req, res);
+});
+
+router.get('/google', function (req, res) {
+  // handle with passport
+  res.send('Logging in with Google');
 });
 
 module.exports = router;
